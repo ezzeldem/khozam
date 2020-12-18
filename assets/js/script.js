d41cdbce64fb2c8bdea0c86665468af3fa90fcbe
ezzelden <ezzelden06@gmail.com>
2020-12-18 02:49:59 +0200

    eezz

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,16 @@ $(document).ready(function () {
     // intro slider
 
     $(".intro-slider").slick({
-        infinite: true,
+        infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
-        // autoplay:true,
+        autoplay:true,
         dots: true,
         autoplaySpeed: 1000,
     });
 
-    // opneMnue
+5  // opneMnue
     $(".nav-section .menu-icon i").click(function () {
         $(".nav-section .nav-link").addClass("opne-menu");
         $("body").addClass("body-over");
@@ -37,13 +37,13 @@ $(document).ready(function () {
     // floorPlan-slider
 
     $(".floorPlan-slider").slick({
-        infinite: true,
+        infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay:true,
+        autoplay:true,
         adaptiveHeight: true,
         dots: false,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 1500,
         prevArrow: "<div class='arrow-slider-header left-arrow'> <i class='fas fa-arrow-left'></i></div>",
         nextArrow: "<div class='arrow-slider-header right-arrow'> <i class='fas fa-arrow-right'></i></div>",
     });
@@ -54,46 +54,46 @@ $(document).ready(function () {
         infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay:true,
+        autoplay:true,
         adaptiveHeight: true,
         dots: false,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 1500,
         prevArrow: "<div class='arrow-slider-header left-arrow'> <i class='fas fa-arrow-left'></i></div>",
         nextArrow: "<div class='arrow-slider-header right-arrow'> <i class='fas fa-arrow-right'></i></div>",
     });
     // gallaery-slider-slider
 
     $(".construction-slider").slick({
-        infinite: true,
+        infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay:true,
+        autoplay:true,
         adaptiveHeight: true,
         dots: false,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 1500,
         prevArrow: "<div class='arrow-slider-header left-arrow'> <i class='fas fa-arrow-left'></i></div>",
         nextArrow: "<div class='arrow-slider-header right-arrow'> <i class='fas fa-arrow-right'></i></div>",
     });
 
     $(".project-slider").slick({
-        infinite: true,
+        infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay:true,
+        autoplay:true,
         adaptiveHeight: true,
         dots: false,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 1500,
         prevArrow: "<div class='arrow-slider-header left-arrow'> <i class='fas fa-arrow-left'></i></div>",
         nextArrow: "<div class='arrow-slider-header right-arrow'> <i class='fas fa-arrow-right'></i></div>",
     });
     $(".news-slider").slick({
-        infinite: true,
+        infinite: false,
         slidesToShow: 2,
         slidesToScroll: 1,
-        // autoplay:true,
+        autoplay:true,
         adaptiveHeight: true,
         dots: false,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 1500,
         prevArrow: "<div class='arrow-slider-header left-arrow'> <i class='fas fa-arrow-left'></i></div>",
         nextArrow: "<div class='arrow-slider-header right-arrow'> <i class='fas fa-arrow-right'></i></div>",
         responsive: [
